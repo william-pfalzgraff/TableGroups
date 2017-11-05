f2f16a991980ea34f5f7c2cbab424ed49aa88bbf
Will Pfalzgraff <wcp@Wills-MacBook-Pro.local>
2017-11-05 07:34:32 -0800

    Bug fix in cost function for minimum table mean.

--- tablegroups/Cost.py
+++ tablegroups/Cost.py
@@ -81,5 +81,5 @@ class BasicCost(BaseCost):
         if min_difference < self._threshold['min_difference']:
             score += 8*(min_difference - self._threshold['min_difference'])
         if mean < self._threshold['mean']:
-            score += 6*(self._threshold['mean'] - mean)
+            score += 6*(mean - self._threshold['mean'])
         return score
